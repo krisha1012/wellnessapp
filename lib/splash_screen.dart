376ee94ea6a98a1875e668855a98e7e551b4bd38
krisha1012 <krishabhanushali.1012@gmail.com>
2024-09-04 23:48:27 +0530

    splashscreen, login and registeration

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -21,22 +21,31 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/logo.png', height: 120), // Your app logo
-            SizedBox(height: 20),
-            Text(
-              'Welcome to Your App',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            SpinKitFadingCircle( // Optional: Loading indicator
-              color: Colors.blue,
-              size: 50.0,
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFFAAD3E0), Color(0xFFffffff)], // Gradient from #AAD3E0 to white
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/logo.png', height: 120), // Your app logo
+              SizedBox(height: 20),
+              Text(
+                'Welcome to Your App',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              SpinKitFadingCircle( // Optional: Loading indicator
+                color: Colors.blue,
+                size: 50.0,
+              ),
+            ],
+          ),
         ),
       ),
     );
